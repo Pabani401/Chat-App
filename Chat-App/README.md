cb964731b970447bb485e5b355d577be042ca6cb
Pabani401 <150105845+Pabani401@users.noreply.github.com>
2024-06-03 09:08:14 +0530

    first commit

---
+++ Chat-App/README.md
@@ -0,0 +1,77 @@
+
+# Talk-A-Tive
+
+Talk-a-tive is a Full Stack Chatting App.
+Uses Socket.io for real time communication and stores user details in encrypted format in Mongo DB Database.
+## Tech Stack
+
+**Client:** React JS
+
+**Server:** Node JS, Express JS
+
+**Database:** Mongo DB
+  
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone 
+```
+
+Go to the project directory
+
+```bash
+  cd mern-chat-app
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+```bash
+  cd frontend/
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+Start the Client
+
+```bash
+  //open now terminal
+  cd frontend
+  npm start
+```
+
+  
+# Features
+
+### Authenticaton
+
+### Real Time Chatting with Typing indicators
+
+### One to One chat
+
+### Search Users
+
+### Create Group Chats
+
+### Notifications 
+
+### Add or Remove users from group
+
+### View Other user Profile
+
+## Made By
+
+
+
+  
---
+++ Chat-App/backend/data/data.js
@@ -0,0 +1,108 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "John",
+        email: "john@example.com",
+      },
+      {
+        name: "Pabani",
+        email: "pabani@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Pabani",
+        email: "pabani@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Anthony",
+        email: "anthony@example.com",
+      },
+      {
+        name: "Pabani",
+        email: "pabani@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anthony",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John",
+        email: "jon@example.com",
+      },
+      {
+        name: "Pabani",
+        email: "pabani@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Jane",
+        email: "jane@example.com",
+      },
+      {
+        name: "Pabani",
+        email: "pabani@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John",
+        email: "john@example.com",
+      },
+      {
+        name: "Pabani",
+        email: "pabani@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+];
